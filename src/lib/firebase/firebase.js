c0413372f8f7c7b5e20e1e726de02595a5bd8ea2
Nikita Nekhay <ktofreesapiens@gmail.com>
2023-04-27 11:30:49 +0300

    1\

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_SENDERID,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDERID,
   appId: import.meta.env.VITE_APPID,
   measurementId: import.meta.env.VITE_MEASUREMENTID
 };
