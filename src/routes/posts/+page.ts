55c2e536d03db8b733165694d71f73598caa82c3
Nikita Nekhay <ktofreesapiens@gmail.com>
2023-06-09 02:04:12 +0300

    @type to ts files

--- src/routes/posts/+page.ts
+++ src/routes/posts/+page.ts
@@ -1,6 +1,6 @@
 import { getBlogPost } from './post';
 
-
+/** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
     const post = await getBlogPost(params.id); // Fetch the blog post details
     console.log(post)
