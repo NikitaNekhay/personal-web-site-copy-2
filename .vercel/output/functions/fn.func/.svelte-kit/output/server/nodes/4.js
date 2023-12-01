659055dcf0970b6042908d228e334cf2e5f3049c
NikitaNA <vaper20041337@gmail.com>
2023-12-01 23:35:58 +0300

    try to deploy from another branch

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/4.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/4.js
@@ -3,6 +3,6 @@
 export const index = 4;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/contact/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.a080b697.js","_app/immutable/chunks/scheduler.b3080bb7.js","_app/immutable/chunks/index.46c9a90c.js","_app/immutable/chunks/runtime.esm.133179ec.js","_app/immutable/chunks/index.52e2593e.js","_app/immutable/chunks/paths.8357d7d2.js"];
+export const imports = ["_app/immutable/nodes/4.d495e8b9.js","_app/immutable/chunks/scheduler.b3080bb7.js","_app/immutable/chunks/index.46c9a90c.js","_app/immutable/chunks/runtime.esm.133179ec.js","_app/immutable/chunks/index.52e2593e.js","_app/immutable/chunks/paths.f0eebbc9.js"];
 export const stylesheets = ["_app/immutable/assets/4.08a8b4c1.css"];
 export const fonts = [];
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,6 +7,8 @@
   import Login from '../../components/MainPages/Login.svelte'
 </script>
 
+
+
 <Login />
 
 <style>
