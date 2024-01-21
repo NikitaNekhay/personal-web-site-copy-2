3bdbb8b551fd926c7637de37edab442acba0d244
NikitaNA <vaper20041337@gmail.com>
2024-01-21 20:49:23 +0300

    og 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 
 <svelte:head>
   <title> NEKHAY NIKITA - CLOTHES SHOP</title>
+  <meta property="og:description" content="Take a look at last collection, shop. Get to know NIKITA entering about and contact pages." />
   <meta name="description" content={$t("Internet shop of clothes designer NIKITA NEKHAY. Home page represents NIKITA'S photo portfolio.")} />
 </svelte:head>
 
