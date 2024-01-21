aa726645a7ff9a6fc34a259bad94d662e71bb7fe
NikitaNA <vaper20041337@gmail.com>
2024-01-21 21:15:14 +0300

    fix robots

--- src/hooks/createRobotsFile.js
+++ src/hooks/createRobotsFile.js
@@ -1,16 +1,41 @@
 import robotstxt from "generate-robotstxt";
 
+// Define the base path for your routes (change this if it's dynamic or defined elsewhere)
+//const base = ''; // Set your base path. For example, '/' or '/my-app'
+
+// Define the admin routes
+const AdminRoutes = [
+    `/dashboard`,
+    `/stat`,
+    `/create`,
+    `/posts`,
+];
+
 // Асинхронный импорт модуля fs
 import('fs').then(fs => {
   robotstxt({
       policy: [
         {
           userAgent: "*",
-          allow: "/",
-          disallow: [],
+          disallow: [
+            ...AdminRoutes, // Spread the admin routes to disallow them
+            "/cgi-bin/",
+            "/wp-admin/",
+            "/?s=", // Disallow search pages
+            "/login/",
+            "/profile/",
+            "/shoppingcart/",
+          ],
+          allow: [
+            "/*.js", // Allow JS files
+            "/*.css", // Allow CSS files
+            "/*.png", // Allow PNG images
+            "/*.jpg", // Allow JPG images
+            "/*.jpeg", // Allow JPEG images
+            "/*.gif", // Allow GIF images
+          ],
           crawlDelay: 2,
         },
-        // ... (если у вас есть специфические правила для других ботов, их можно оставить здесь)
       ],
       sitemap: "http://nekhaynikita.shop/sitemap.xml", // Укажите правильный URL к вашему sitemap
       host: "http://nekhaynikita.shop", // Укажите основной домен вашего сайта
